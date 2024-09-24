650eb217887968264d6ad71d39f84911623c1250
Thom Cameron <thomcm@proton.me>
2024-09-24 13:47:02 +0100

    Update default.nix for v0.3.0

--- default.nix
+++ default.nix
@@ -5,16 +5,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "isw";
-  version = "0.2.0";
+  version = "0.3.0";
 
   src = fetchFromGitLab {
     owner = "thom-cameron";
     repo = pname;
     rev = version;
-    hash = "sha256-PrdF97bC7SyZC7ReN5hLREccTRbifXiatuzHkQbk04k=";
+    hash = "sha256-8DbRTJheWt1hlwNW566rnw9hccXajEqhQ/IZsb8cDa4=";
   };
 
-  cargoHash = "sha256-TeCPgVC7iPKnUfZxMJFTKuOF8bLuh3NSpcmm0VZEr+g=";
+  cargoHash = "sha256-LpKiP7bwiNEUXYvQsUG//wqOwGmg2UD3DA0Gb2h7kMw=";
 
   meta = with lib; {
     description = "a simple terminal stopwatch application";
