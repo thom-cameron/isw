e58235fd491df6b368840f61dbb39ebe58585e06
Thom Cameron <thomcm@proton.me>
2024-09-26 16:35:00 +0100

    Add some interval unit tests

--- src/intervals.rs
+++ src/intervals.rs
@@ -13,10 +13,10 @@ pub struct IntervalList {
 
 impl IntervalList {
     pub fn new(
-        intervals_string: String,
+        durations_string: String,
         optional_colours_string: Option<String>,
     ) -> Result<Self, io::Error> {
-        let durations = Self::parse_int_string::<u32>(intervals_string)?;
+        let durations = Self::parse_int_string::<u32>(durations_string)?;
 
         let optional_colours: Option<Vec<u8>> = match optional_colours_string {
             Some(colours_string) => Some(Self::parse_int_string::<u8>(colours_string)?),
@@ -115,6 +115,82 @@ impl Interval {
                 7 => Color::White,
                 _ => Color::White,
             },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn int_vec_to_string<T>(vec: Vec<T>) -> String
+    where
+        T: FromStr + ToString,
+        T::Err: std::fmt::Display,
+    {
+        vec.iter()
+            .map(|duration| duration.to_string())
+            .collect::<Vec<String>>()
+            .join(",")
+    }
+
+    #[test]
+    fn valid_configurations() {
+        for (durations, colours) in [
+            (vec![60], None),
+            (vec![30; 100], None),
+            (vec![60], Some(vec![7])),
+            (vec![30, 30, 30], Some(vec![1, 2, 3])),
+        ] {
+            // construct a list of intervals from the raw values
+            let colours = match colours {
+                Some(colours) => colours,
+                None => IntervalList::generate_colours(durations.len()),
+            };
+            let raw_interval_list: Vec<Interval> = durations
+                .iter()
+                .zip(colours.iter())
+                .map(|(&duration, &colour)| Interval::new(duration, colour))
+                .collect();
+
+            // convert the raw values to strings and construct an IntervalList
+            let parsed_interval_list =
+                IntervalList::new(int_vec_to_string(durations), Some(int_vec_to_string(colours)))
+                    .expect("Failed to parse the test interval configuration strings");
+
+            // compare the resulting Interval instances
+            for (raw_interval, parsed_interval) in raw_interval_list
+                .iter()
+                .zip(parsed_interval_list.intervals.iter())
+            {
+                assert_eq!(raw_interval.duration, parsed_interval.duration);
+                assert_eq!(raw_interval.colour, parsed_interval.colour);
+            }
+        }
+    }
+
+    #[test]
+    fn invalid_configurations() {
+        for (durations_string, colours_string) in [
+            (",".to_string(), None),
+            ("30,".to_string(), None),
+            ("30".to_string(), Some(",".to_string())),
+            ("30".to_string(), Some("1,".to_string())),
+        ] {
+            let result = IntervalList::new(durations_string, colours_string);
+
+            assert!(result.is_err());
+        }
+    }
+
+    #[test]
+    fn generate_colours() {
+        for (durations, correct_colours) in
+            [(vec![60], vec![1]), (vec![30; 8], vec![1, 2, 3, 4, 5, 6, 7, 1])]
+        {
+            let generated_colours = IntervalList::generate_colours(durations.len());
+
+            assert_eq!(correct_colours, generated_colours);
         }
     }
 }
